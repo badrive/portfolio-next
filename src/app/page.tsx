a10cee05c7ba3d7ce273cf2b09b6cbc2b54613d8
badrive <badirov.mimi@gmail.com>
2025-02-23 21:38:42 +0000

    skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@
 import Image from "next/image";
 import Hero from "./sections/hero"
 import Dock from "./sections/dock"
+import Magnet from "./sections/magnet"
+import Skills from "./sections/skills"
 
 export default function Home() {
   return (
     <>
       <Dock />
       <Hero />
+      <Magnet />
+      <Skills />
     </>
   );
 }
